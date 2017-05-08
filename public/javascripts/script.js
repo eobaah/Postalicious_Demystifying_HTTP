b7e14a3e5efb5fdc4ec5eb454947edfc997c67c1
eobaah <eobaah@gmail.com>
2017-05-08 16:15:08 -0700

    get HTTP verb value from form

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,8 @@
+// window.onload = selectTab();
+
+
 function selectTab(event, reqres) {
+   console.log(event)
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -11,3 +15,9 @@ function selectTab(event, reqres) {
     document.getElementById(reqres).style.display = "block";
     event.currentTarget.className += " active";
 }
+
+let httpVerb = document.querySelector('.http-verb').value
+console.log(httpVerb)
+// check where the data is
+// get the data from the input fields
+// construct a request object using that data
