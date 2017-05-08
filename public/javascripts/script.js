8def50cb6d464d0fc2355016a0342dd4ced8b371
eobaah <eobaah@gmail.com>
2017-05-08 14:53:29 -0700

    created the basic layout for our app

---
+++ public/javascripts/script.js
@@ -0,0 +1,13 @@
+function selectTab(event, reqres) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(reqres).style.display = "block";
+    event.currentTarget.className += " active";
+}
